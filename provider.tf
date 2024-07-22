579e09d3b48b7af7efe42142391b888b27a5c479
yesdani <yesdaniece@gmail.com>
2024-07-22 02:16:07 +0100

    example

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.29.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "roboshop-remote-state-yesdani"
+    key    = "tfvars"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-dynamotable"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+
+  region = "us-east-1"
+}
